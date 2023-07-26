a5f136e7002532de9d199b95424cb62adb51aa35
TeamxAlpha <zohaib104006@gmail.com>
2023-07-26 17:08:43 +0500

    add the products through API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Store App</h1>
       </header>
+      <h2>Products</h2>
+      <div className="product-list">
+        {products.map((product) => (
+          <div key={product.id} className="product">
+            <h2 className="product-title">{product.title}</h2>
+            <p className="product-description">{product.description}</p>
+            <p className="product-price">Price: ${product.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
